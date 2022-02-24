4625dff7cd501bb3de3381644cd1539df9f0988f
Olha Gruzglina <olga.zhmura@gmail.com>
2022-02-23 20:58:17 -0500

    commit tree e1529704d8a6e6ce118a1fd9fdfc3d6e9e0e292b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.filter {|array_word| array_word.chars.sort == @word.chars.sort}
+    end
+end
